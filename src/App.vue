94a302b4924e31b2369e5f6a96fdacfea5ef316a
Ryan Hunter <ryanhunterjnr@gmail.com>
2021-06-14 02:20:29 -0400

    Mass upload for basic templating and setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Twitter Clone</router-link> | 
+    <router-link to="/about">About Us</router-link> |  
+    <router-link to="/contact">Contact Us</router-link> 
+  </div>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #2aa9e0;
+}
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contact">
+    <div className="container">
+      <h1 className="is-size-2 has-text-weight-bold has-text-link-dark  has-text-centered">Contact Us</h1>
+      <img alt="Contact Us" src="../assets/contact_us_01.png" />
+      <br /><br />
+      <p class="has-text-justified">
+        When does this sunset? I have a hard stop in an hour and half ... Can we
+        align on lunch orders move the needle so open door policy, yet let's
+        pressure test this, mobile friendly increase the resolution, scale it up
+        we need a larger print. The last person we talked to said this would be
+        ready
+      </p>
+      <br />
+    </div>
+  </div>
+</template>
